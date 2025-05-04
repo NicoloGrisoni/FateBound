a8d93c81d2486657e422f43a98b1f3a409143033
nicog <grisoninicolo@gmail.com>
2025-05-04 11:13:41 +0200

    Import of the TailwindCSS compiled file in each of the HTML pages

--- public/login/registration.php
+++ public/login/registration.php
@@ -10,9 +10,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registration</title>ù
+
+  <!-- Google Font: Poppins -->
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+    rel="stylesheet"
+  />
+
+  <link rel="stylesheet" href="../build/tailwind.css"/>
 </head>
 <body>
   <!-- 
