ba3c2b15466044bf6ec6c2f3b3039e5e52d752d5
nicog <grisoninicolo@gmail.com>
2025-05-07 21:39:59 +0200

    Fixing error about Unsplash API

--- public/js/game.js
+++ public/js/game.js
@@ -25,10 +25,37 @@ async function GetChapter() {
                 let jsonChapter = JSON.parse(textChapter)
                 console.log(jsonChapter)
             }
+
+            SetImageUnsplah(json["chapter"].title + json["chapter"].description)
+            document.getElementById("title").innerHTML = json["chapter"].title
+            document.getElementById("description").innerHTML = json["chapter"].description
         } else {
             console.log(response.status)
         }
     } catch (error) {
         console.log(error)
+    }
+}
+
+async function SetImageUnsplah(search) {
+    const UnsplashApiKey = "Y033JaPjRjSuKxiNB4mTmWq_RWcW7-65ETq7lZ9sotE";
+    const unsplashUrl = `https://api.unsplash.com/search/photos?query=${encodeURIComponent(search)}&per_page=1&order_by=relevant&lang=it`;
+
+    const responseImage = await fetch(unsplashUrl, {
+        headers: {
+            'Authorization': `Client-ID ${UnsplashApiKey}`
+        }
+    });
+
+    if (!responseImage.ok) {
+        console.error(`Unsplash API error: ${responseImage.status}`);
+    } else {
+        const dataImage = await responseImage.json();
+        if (dataImage.results && dataImage.results.length > 0) {
+            const imageUrl = dataImage.results[0].urls.small;
+            document.getElementById("image").src = imageUrl;
+        } else {
+            console.warn("Nessuna immagine trovata per:", search);
+        }
     }
 }
